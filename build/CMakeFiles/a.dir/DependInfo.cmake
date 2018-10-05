82fadaf4609bb515019e1c3ec29d9d79e50c56b9
Wes <wjthorburn@live.com.au>
2018-10-05 13:44:16 -0500

    Switch to CMake build process

---
+++ build/CMakeFiles/a.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wes/Desktop/projects/cross-platform/src/ActivePlayer.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/ActivePlayer.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/Browser.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/Browser.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/Camera.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/Camera.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/Canvas.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/Canvas.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/Controls.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/Controls.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/DebugVariables.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/DebugVariables.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/Game.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/Game.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/Hud.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/Hud.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/Map.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/Map.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/Player.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/Player.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/RenderTimer.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/RenderTimer.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/Timer.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/Timer.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/TouchControls.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/TouchControls.cpp.o"
+  "/home/wes/Desktop/projects/cross-platform/src/main.cpp" "/home/wes/Desktop/projects/cross-platform/build/CMakeFiles/a.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/a.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.dir/src/Canvas.cpp.o"
+  "CMakeFiles/a.dir/src/TouchControls.cpp.o"
+  "CMakeFiles/a.dir/src/RenderTimer.cpp.o"
+  "CMakeFiles/a.dir/src/Game.cpp.o"
+  "CMakeFiles/a.dir/src/Browser.cpp.o"
+  "CMakeFiles/a.dir/src/Timer.cpp.o"
+  "CMakeFiles/a.dir/src/main.cpp.o"
+  "CMakeFiles/a.dir/src/Map.cpp.o"
+  "CMakeFiles/a.dir/src/Camera.cpp.o"
+  "CMakeFiles/a.dir/src/DebugVariables.cpp.o"
+  "CMakeFiles/a.dir/src/Controls.cpp.o"
+  "CMakeFiles/a.dir/src/Player.cpp.o"
+  "CMakeFiles/a.dir/src/Hud.cpp.o"
+  "CMakeFiles/a.dir/src/ActivePlayer.cpp.o"
+  "../public/a.pdb"
+  "../public/a.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
